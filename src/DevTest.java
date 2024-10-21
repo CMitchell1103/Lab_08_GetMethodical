f7bf42db94b6c945f3d358998796a29820f8e011
CMitchell1103 <mitch3cd@mail.uc.edu>
2024-10-21 19:33:36 -0400

    Methods 1 2 and 3

---
+++ src/DevTest.java
@@ -0,0 +1,119 @@
+import java.util.Scanner;
+
+public class DevTest {
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        /*
+        String name = getNonZeroLenString(in, "Enter your name");
+        System.out.println("Name is " + name);
+
+
+        int age = getInt(in, "Enter your age ");
+        System.out.println("Your said your age is " + age);
+
+         */
+
+        double salary = getDouble(in, "Enter your salary ");
+        System.out.println("You said your salary was " + salary);
+    }
+
+    /**
+     * get a string val form the user which must be at least one character
+     *
+     * @param pipe scanner to use to read the input
+     * @param prompt prompt to tell the user what is input
+     * @return String that is at least one character
+     */
+
+    public static String getNonZeroLenString(Scanner pipe, String prompt)
+    {
+        String retVal = "";
+
+        do {
+            System.out.print(prompt + ": ");
+            retVal = pipe.nextLine();
+            if(retVal.isEmpty())
+            {
+                System.out.println("You must enter at least one character!");
+            }
+
+        } while(retVal.isEmpty());
+
+        return retVal;
+
+    }
+
+    /**
+     * get an unconstrained int value from the user
+     *
+     * @param pipe Scanner to use for input
+     * @param prompt prompt that tells the user what to input
+     * @return an unconstrained value
+     */
+    public static int getInt(Scanner pipe, String prompt)
+
+    {
+            int retVal = 0;
+            String trash = "";
+            boolean done = false;
+
+            do {
+                System.out.print(prompt + ": ");
+                if(pipe.hasNextInt()) {
+                    retVal = pipe.nextInt();
+                    pipe.nextLine();
+                    done = true;
+                }
+                else
+                {
+                    trash = pipe.nextLine();
+                    System.out.println("You must enter a valid integer not " + trash);
+                }
+
+            } while(!done);
+
+            return retVal;
+
+    }
+
+    /**
+     * get an unconstrained double value from the user
+     *
+     * @param pipe Scanner to use for input
+     * @param prompt prompt that tells the user what to input
+     * @return an unconstrained double value
+     */
+    public static double getDouble(Scanner pipe, String prompt)
+
+    {
+        double retVal = 0;
+        String trash = "";
+        boolean done = false;
+
+        do {
+            System.out.print(prompt + ": ");
+            if(pipe.hasNextDouble())
+            {
+                retVal = pipe.nextDouble();
+                pipe.nextLine();
+                done = true;
+            }
+            else
+            {
+                trash = pipe.nextLine();
+                System.out.println("You must enter a valid double not " + trash);
+            }
+
+        } while(!done);
+
+        return retVal;
+
+    }
+
+
+
+
+}
