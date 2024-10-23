4d21d143a644f52100e3b23f3b0fc6dc0f76dc04
CMitchell1103 <mitch3cd@mail.uc.edu>
2024-10-22 21:54:01 -0400

    Prog 2

--- src/FavNum.java
+++ src/FavNum.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class FavNum {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int favoriteNum = 0;
+        String trash = "";
+        boolean done = false;
+        double favNum = 0;
+        favoriteNum = SafeInput.getInt(in, "Enter your favorite int");
+        favNum = SafeInput.getDouble(in, "Enter your favorite double");
+        System.out.println("Your favorite int is: " + favoriteNum + "\nYour favorite double is: " + favNum);
+
+
+
+    }
 }
