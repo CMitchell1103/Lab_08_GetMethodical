31b078aee44f02ef008331bf8b57c6d713b42268
CMitchell1103 <mitch3cd@mail.uc.edu>
2024-10-23 17:53:55 -0400

    Prog reggie

--- src/CheckOut.java
+++ src/CheckOut.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class CheckOut {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        double price = 0;
+        String trash = "";
+        boolean done = false;
+        String input = "";
+        boolean moreItems  = false;
+
+
+        price = SafeInput.getRangedDouble(in, "Enter the price of your item ", .50,10.00);
+        moreItems = SafeInput.getYNConfirm(in, "Do you have more items?");
+
+
+
+
+
+
+    }
 }
