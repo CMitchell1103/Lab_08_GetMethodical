07a156907f488842f112aa13b6ba9cc4f7958dd8
CMitchell1103 <mitch3cd@mail.uc.edu>
2024-10-23 20:27:53 -0400

    CtpFTable

--- src/PrettyHeader.java
+++ src/PrettyHeader.java
@@ -1,2 +1,39 @@
-public class PrettyHeader {
+
+public class PrettyHeader
+{
+    public static void main(String[] args)
+    {
+        prettyHeader("Message Centered Here");
+    }
+    /**
+     *
+     * @param msg title for header
+     */
+    public static void prettyHeader(String msg)
+    {
+        int width = 60;
+        for (int i = 0; i < width; i++)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+        int msgLen = msg.length();
+        int space = (width - msgLen - 6) / 2;
+        System.out.println("***");
+        for (int i = 0; i < space; i++)
+        {
+            System.out.print(" ");
+        }
+        System.out.print(msg);
+        for (int i = 0; i < space; i++)
+        {
+            System.out.print(" ");
+        }
+        System.out.println("***");
+        for(int i = 0; i < width; i++)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
 }
