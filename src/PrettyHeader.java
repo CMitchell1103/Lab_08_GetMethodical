200d5fb74c0d9fda52fd56f5819d7286d3ea9d7b
CMitchell1103 <mitch3cd@mail.uc.edu>
2024-10-23 21:15:55 -0400

    pretty header prog

--- src/PrettyHeader.java
+++ src/PrettyHeader.java
@@ -1,10 +1,7 @@
 
 public class PrettyHeader
 {
-    public static void main(String[] args)
-    {
-        prettyHeader("Message Centered Here");
-    }
+
     /**
      *
      * @param msg title for header
@@ -12,28 +9,40 @@ public class PrettyHeader
     public static void prettyHeader(String msg)
     {
         int width = 60;
+
         for (int i = 0; i < width; i++)
         {
             System.out.print("*");
         }
+
         System.out.println();
+
         int msgLen = msg.length();
-        int space = (width - msgLen - 6) / 2;
-        System.out.println("***");
-        for (int i = 0; i < space; i++)
+        int space = width - msgLen - 6;
+        int leftSpace = space / 2;
+        int rightSpace = space - leftSpace;
+
+        System.out.print("***");
+        for (int i = 0; i < leftSpace; i++)
         {
             System.out.print(" ");
         }
         System.out.print(msg);
-        for (int i = 0; i < space; i++)
+        for (int i = 0; i < rightSpace; i++)
         {
             System.out.print(" ");
         }
         System.out.println("***");
+
         for(int i = 0; i < width; i++)
         {
             System.out.print("*");
         }
         System.out.println();
+    }
+
+    public static void main(String[] args)
+    {
+        prettyHeader("Message Centered Here");
     }
 }
